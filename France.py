1455bb5c27926735098a31074a70d1494e989182
ConnorF94 <74943730+ConnorF94@users.noreply.github.com>
2020-11-26 15:37:19 -0600

    Update France.py

--- France.py
+++ France.py
@@ -1,2 +1,16 @@
 #First change/addition to .py file
 Print "Welcome to Connor's GitHub Python File."
+# Input taxable income
+print ("What is your taxable income?")
+incomeString = input()
+income = int (incomeString)
+# Test againt local tax rate
+if income <= 20000:
+    tax = 0.02 * income
+else:
+    if income <= 50000:
+        tax = 400 + 0.025 * (income - 20000)
+    else:
+        tax = 1150 + 0.035 * (income - 50000)
+print ("Your income tax is: $%.2f" %tax)
+
